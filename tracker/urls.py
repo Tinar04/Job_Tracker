6d675e9d25b9a0c07c400387887b61901fd9298c
Tinar04 <rathoretina76@gmail.com>
2026-05-12 23:37:49 +0530

    fix: fixed the url of contact path

--- tracker/urls.py
+++ tracker/urls.py
@@ -5,7 +5,7 @@ from tracker import views
 urlpatterns = [
     path("", views.index,name='home'),
     path("about-us/", views.about,name = 'about'),
-    path('contact',views.contact,name='contactus'),
+    path('contact/',views.contact,name='contactus'),
     path('status/<str:status>/', views.jobs_by_status, name='jobs_by_status'),
     path('add_job/',views.add_job,name = 'Add_Job'),
     path('register/',views.register,name = 'register'),
