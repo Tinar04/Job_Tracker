88f29c329ffb8d13c4e3f7df206820e7c4791669
Tinar04 <rathoretina76@gmail.com>
2026-05-12 17:46:44 +0530

    fix: the login url

--- config/urls.py
+++ config/urls.py
@@ -21,6 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tracker.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
